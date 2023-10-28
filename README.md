a12f5385be074af575fbb9254a60c3cf5614da27
arlan lloyd <arlanlloyd@gmail.com>
2023-10-28 13:06:34 -0700

    add example

---
+++ README.md
@@ -0,0 +1,2 @@
+# nixplayground
+My exploration into the Nix language and creating references along the way.
---
+++ examples/python-example/default.nix
@@ -0,0 +1,21 @@
+let
+  pkgs = import <nixpkgs> {};
+  build = pkgs.python3Packages.callPackage ./package.nix {};
+in
+  {
+    inherit build;
+
+    shell = pkgs.mkShell {
+      inputsFrom = [ build ];
+
+      packages = with pkgs; [
+        curl
+        hurl
+      ];
+
+      pythonpackages = with pkgs.python3Packages; [
+        black
+        flake8
+      ];
+    };
+  }
