c1557bbbe676a37d6345e4b2d820b2e872504048
arlan lloyd <arlanlloyd@gmail.com>
2023-11-13 00:05:19 -0800

    add working flake/home manager

--- wsl/flake.nix
+++ wsl/flake.nix
@@ -4,28 +4,40 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
   inputs.nixoswsl.url = "github:nix-community/NixOS-WSL";
   inputs.nixoswsl.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-23.05";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
   inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
 
   outputs = { 
-    self,
     nixpkgs,
     nixoswsl,
+    home-manager,
     vscode-server,
     ...
     }: {
-
-
+    #nixos configs
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         nixoswsl.nixosModules.wsl
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nixos = import ./home.nix;
+        }
         vscode-server.nixosModules.default
         ({ pkgs, ... }: {
           system = {
             stateVersion = "23.05";
           };
           environment.systemPackages = [
+            pkgs.btop
+            pkgs.htop
             pkgs.wget
+            pkgs.jq
+            pkgs.yq-go
+            pkgs.dnsutils
           ];
 
           wsl = {
